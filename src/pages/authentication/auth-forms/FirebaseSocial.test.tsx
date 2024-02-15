e70fcf096cd0d996859a633bd9811d0485b023c8
jnakaso <jnakaso@yahoo.com>
2024-02-15 09:13:47 -1000

    component testing

---
+++ src/pages/authentication/auth-forms/FirebaseSocial.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react';
+import { afterEach, assert, describe, expect, it, vi } from 'vitest';
+import { authService } from '../../../services/authService';
+import FirebaseSocial from './FirebaseSocial';
+import { useMediaQuery, useTheme } from '@mui/material';
+
+// ==============================|| FIREBASE - SOCIAL BUTTON ||============================== //
+
+
+describe('FirebaseSocial tests', () => {
+  const signInWithGoogleSpy = vi.spyOn(authService, 'signInWithGoogle');
+
+  it('should render the app', () => {
+    const element = render(<FirebaseSocial />);
+    assert.isNotNull(element.queryByText('Google'));
+    assert.isNotNull(element.queryByText('Facebook'));
+  });
+
+  it('Google button should work', () => {
+    const element = render(<FirebaseSocial />);
+    const button = element.queryByTitle('Login with Google');
+    button?.click();
+    expect(signInWithGoogleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Facebook button should not work', () => {
+    const element = render(<FirebaseSocial />);
+    const button = element.queryByText('Facebook');
+    button?.click();
+    expect(signInWithGoogleSpy).toHaveBeenCalledTimes(0);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+});
+
--- src/pages/authentication/auth-forms/FirebaseSocial.tsx
+++ src/pages/authentication/auth-forms/FirebaseSocial.tsx
@@ -12,7 +12,6 @@ import { authService } from '../../../services/authService';
 const FirebaseSocial = () => {
   const theme = useTheme();
   const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
-
   const googleHandler = async () => {
     authService.signInWithGoogle()
       .then(resp => console.log(resp))
@@ -30,22 +29,22 @@ const FirebaseSocial = () => {
       sx={{ '& .MuiButton-startIcon': { mr: matchDownSM ? 0 : 1, ml: matchDownSM ? 0 : -0.5 } }}
     >
       <Button
+        title='Login with Google'
         variant="outlined"
         color="secondary"
         fullWidth={!matchDownSM}
         startIcon={<img src={Google} alt="Google" />}
-        onClick={googleHandler}
-      >
+        onClick={googleHandler}>
         {!matchDownSM && 'Google'}
       </Button>
 
       <Button
+        title='Login with Facebook'
         variant="outlined"
         color="secondary"
         fullWidth={!matchDownSM}
         startIcon={<img src={Facebook} alt="Facebook" />}
-        onClick={facebookHandler}
-      >
+        onClick={facebookHandler}>
         {!matchDownSM && 'Facebook'}
       </Button>
     </Stack>
